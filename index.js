a458ae92344e4abcadfbfedff2827900eb0bfe58
Peter Kuzmenko <petr.kuzmenko.04@gmail.com>
2023-02-12 21:47:12 +0200

    add copy button

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const input = document.getElementById('input');
 const submit = document.getElementById('submit');
+const copyButton = document.getElementById('copy');
 const output = document.getElementById('output');
 const chapter = document.getElementById('chapter');
 
@@ -14,3 +15,16 @@ submit.addEventListener('click', () => {
 
     output.innerText = str;
 });
+
+const copyContent = async (text) => {
+    try {
+        await navigator.clipboard.writeText(text);
+        console.log('Content copied to clipboard');
+    } catch (err) {
+        console.error('Failed to copy: ', err);
+    }
+}
+
+copyButton.addEventListener('click', () => {
+    copyContent(output.innerText).catch(null);
+})
